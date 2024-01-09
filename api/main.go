d86be85d665d07271d2e6c73cc75d79fbb85a35f
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-09 09:16:43 -0300

    using gorilla mux to validate request type

--- api/main.go
+++ api/main.go
@@ -7,22 +7,21 @@ import (
 	"net/http"
 
 	models "github.com/Serinolli/scraper-api/models"
+	"github.com/gorilla/mux"
 )
 
 var port string = "8000"
 
 func main() {
-	fmt.Println("Listening to port " + port)
-	http.HandleFunc("/posts", getPosts)
+	muxRouter := mux.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	fmt.Println("Listening to port " + port)
+	muxRouter.HandleFunc("/posts", getPosts).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":"+port, muxRouter))
 }
 
 func getPosts(writer http.ResponseWriter, request *http.Request) {
-	if request.Method != "GET" {
-		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-	}
-
 	writer.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(writer).Encode([]models.Post{{
 		ID:      "sampletest1",
