38b8a0b4e752e7c3728dad4bb1cd1a883d59164c
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-09 22:59:23 -0300

    updating post structure

--- models/Post.go
+++ models/Post.go
@@ -1,8 +1,12 @@
 package model
 
 type Post struct {
-	ID      string `json:"id"`
-	Title   string `json:"title"`
-	Upvotes int    `json:"upvotes"`
-	Content string `json:"content"`
+	ID        string `json:"id"`
+	SubReddit string `json:"subReddit"`
+	Timestamp string `json:"timeStamp"`
+	Author    string `json:"author"`
+	URL       string `json:"url"`
+	Title     string `json:"title"`
+	Upvotes   int    `json:"upvotes"`
+	Content   string `json:"content"`
 }
