791562a18a9b540b92e3bac93bab03cfa097d532
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-16 23:08:26 -0300

    creating post repository and adding first get all func

---
+++ repositories/posts-repository.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	m "github.com/Serinolli/scraper-api/models"
+)
+
+type Server m.Server
+
+func (s *Server) GetAllPosts(w http.ResponseWriter, r *http.Request) {
+	coll := s.Client.Database("redditscrapper").Collection("posts")
+
+	query := bson.M{}
+	cursor, err := coll.Find(context.TODO(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := []bson.M{}
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(results)
+}
