14857d62d740478a9f963cb26353aacf7cf305e4
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-16 23:09:57 -0300

    removing previous static return from endpoint

--- api/main.go
+++ api/main.go
@@ -25,9 +25,11 @@ func main() {
 		panic(err)
 	}
 
+	fmt.Println(client)
+
 	fmt.Println("Listening to port " + port)
 	muxRouter.HandleFunc("/posts", getPosts).Methods("GET")
-	muxRouter.HandleFunc("/posts", createPost).Methods("POST")
+	muxRouter.HandleFunc("/posts", createPosts).Methods("POST")
 	//muxRouter.HandleFunc("/posts/{id}", getPost).Methods("GET")
 	//muxRouter.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
 	//muxRouter.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
@@ -37,15 +39,10 @@ func main() {
 
 func getPosts(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(writer).Encode([]m.Post{{
-		ID:      "sampletest1",
-		Title:   "starting api",
-		Upvotes: 15,
-		Content: "lorem ipsum dolor sit amet",
-	}})
+
 }
 
-func createPost(writer http.ResponseWriter, request *http.Request) {
+func createPosts(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
 
 	var posts []m.Post
@@ -56,4 +53,17 @@ func createPost(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	json.NewEncoder(writer).Encode(posts)
+}
+
+func createPost(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+
+	var post m.Post
+	err := json.NewDecoder(request.Body).Decode(&post)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(writer).Encode(post)
 }
