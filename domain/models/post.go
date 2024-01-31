b26a2045f62357fed23243a2d2279b6138697742
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-30 21:57:00 -0300

    changing timestamp to creation date

--- domain/models/post.go
+++ domain/models/post.go
@@ -1,12 +1,13 @@
 package model
 
+import "time"
+
 type Post struct {
-	PostId    string `json:"postid"`
-	SubReddit string `json:"subReddit"`
-	Timestamp int    `json:"timeStamp"`
-	Author    string `json:"author"`
-	URL       string `json:"url"`
-	Title     string `json:"title"`
-	Upvotes   int    `json:"upvotes"`
-	Content   string `json:"content"`
+	PostId    string    `json:"postid"`
+	SubReddit string    `json:"subReddit"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"createdAt"`
+	Author    string    `json:"author"`
+	URL       string    `json:"url"`
+	Title     string    `json:"title"`
+	Upvotes   int       `json:"upvotes"`
 }
